bc8e54a018b187905e53cd80ff61e74dcb33839d
Sadanand09 <sadanandgurude9@gmail.com>
2025-07-24 15:40:32 +0530

    ref: setup auth & OAuth with github and google

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
+
   return (
     <div>
       <h1 className="h1-bold">Welcome to Next.js👋</h1>
       <h1 className="h1-bold font-space-grotesk">Welcome to Next.js👋</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
--- components/forms/SocialAuthForm.tsx
+++ components/forms/SocialAuthForm.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import React from "react";
-import { Button } from "../ui/button";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
+
+import ROUTES from "@/constants/routes";
+import { toast } from "sonner";
+
+import { Button } from "../ui/button";
 
 const SocialAuthForm = () => {
   const buttonClass =
     "background-dark400_light900 body-medium text-dark200_light800 rounded-2 min-h-12 flex-1 px-4 py-3.5 mx-1";
+
+  const handleSignIn = async (provider: "gitHub" | "google") => {
+    try {
+      await signIn(provider, {
+        callbackUrl: ROUTES.HOME,
+        redirect: false,
+      });
+    } catch (error) {
+      console.log(error);
+
+      toast(
+        <div>
+          <p className="font-semibold">Sign-in Failed</p>
+          <p className="text-sm text-muted-foreground">Something went wrong</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="mt-10 flex flex-wrap gap2.5">
-      <Button className={buttonClass}>
+      <Button className={buttonClass} onClick={() => handleSignIn("gitHub")}>
         <Image
           src="/icons/github.svg"
           alt="Github Logo"
@@ -17,8 +43,8 @@ const SocialAuthForm = () => {
         />
         <span>Login with GitHub</span>
       </Button>
-      
-      <Button className={buttonClass}>
+
+      <Button className={buttonClass} onClick={() => handleSignIn("google")}>
         <Image
           src="/icons/google.svg"
           alt="Google Logo"
