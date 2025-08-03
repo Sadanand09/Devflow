22e5a72bf2654834e33b4d77a04caac750367068
Sadanand09 <sadanandgurude9@gmail.com>
2025-08-03 22:55:51 +0530

    ref: Implement RightSidebar

--- components/navigation/navbar/NavLinks.tsx
+++ components/navigation/navbar/NavLinks.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { SheetClose } from "@/components/ui/sheet";
 import { sidebarLinks } from "@/constants";
@@ -9,34 +9,58 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavLinks = ({ isMobileNav = false }: { isMobileNav?: boolean }) => {
+  const pathname = usePathname();
+  const userId = 1;
 
-    const pathname = usePathname();
-    const userId = 1;
+  return (
+    <>
+      {sidebarLinks.map((item) => {
+        const isActive =
+          (pathname.includes(item.route) && item.route.length > 1) ||
+          pathname == item.route;
 
-    return <>
-        {sidebarLinks.map((item) => {
-            const isActive = (pathname.includes(item.route) && item.route.length > 1) || pathname == item.route;
+        if (item.route === "/profile") {
+          if (userId) item.route = `${item.route}/${userId}`;
+          else return null;
+        }
 
-            if (item.route === '/profile') {
-                if (userId) item.route = `${item.route}/${userId}`;
-                else return null;
-            }
-
-            const LinkComponent = (
-                <Link href={item.route} key={item.label} className={cn(isActive ? 'primary-gradient rounded-lg text-light-900' : 'text-dark300_light900', "flex items-center justify-start gap-4 bg-transparent p-4")}>
-                    <Image src={item.imgURL} alt={item.label} width={20} height={20} className={cn({" invert-colors": !isActive})} />
-                    <p className={cn(isActive ? "base-bold" : "base-medium", !isMobileNav && "max-lg-hidden")}>{item.label}</p>
-                </Link>
-            );
-            return isMobileNav ? (
-                <SheetClose asChild key={item.route}>
-                    {LinkComponent}
-                </SheetClose> 
-            ) : (
-                    <React.Fragment key={item.route}>{ LinkComponent }</React.Fragment>
-            )
-    })}
-    </>;
+        const LinkComponent = (
+          <Link
+            href={item.route}
+            key={item.label}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start gap-4 bg-transparent p-4"
+            )}
+          >
+            <Image
+              src={item.imgURL}
+              alt={item.label}
+              width={20}
+              height={20}
+              className={cn({ " invert-colors": !isActive })}
+            />
+            <p
+              className={cn(isActive ? "base-bold" : "base-medium", {
+                "max-lg:hidden": !isMobileNav,
+              })}
+            >
+              {item.label}
+            </p>
+          </Link>
+        );
+        return isMobileNav ? (
+          <SheetClose asChild key={item.route}>
+            {LinkComponent}
+          </SheetClose>
+        ) : (
+          <React.Fragment key={item.route}>{LinkComponent}</React.Fragment>
+        );
+      })}
+    </>
+  );
 };
 
 export default NavLinks;
