c14c4ebb7f8c6ae24a3149e1954665580181785a
Sadanand09 <sadanandgurude9@gmail.com>
2025-08-25 22:55:19 +0530

    ...

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import React from "react";
+import { signIn } from "next-auth/react";
 
 import AuthForm from "@/components/forms/AuthForm";
 import { SignInSchema } from "@/lib/validations";
-
 
 const SignIn = () => {
   return (
@@ -12,7 +12,19 @@ const SignIn = () => {
       formType="SIGN_IN"
       schema={SignInSchema}
       defaultValues={{ email: "", password: "" }}
-      onSubmit={(data) => Promise.resolve({ success: true, data })}
+      onSubmit={async (data) => {
+        const res = await signIn("credentials", {
+          redirect: false, // so you can handle errors manually
+          email: data.email,
+          password: data.password,
+        });
+
+        if (res?.error) {
+          return { success: false, error: res.error };
+        }
+
+        return { success: true, data: res };
+      }}
     />
   );
 };
