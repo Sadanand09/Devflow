17905e7ef0a8b01813f8ebb6e806e52d5735c1d7
Sadanand09 <sadanandgurude9@gmail.com>
2025-09-04 12:01:31 +0530

    ref: Implement Tags Question Action

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import React from "react";
 import TagCard from "../cards/TagCard";
 
-
 const hotQuestions = [
   { _id: "1", title: "How to create a custom hook in React?" },
   { _id: "2", title: "How to use React Query?" },
@@ -26,6 +25,7 @@ const RightSidebar = () => {
     <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map(({ _id, title }) => (
             <Link
@@ -34,6 +34,7 @@ const RightSidebar = () => {
               className="flex cursor-pointer items-center justify-between gap-7"
             >
               <p className="body-medium text-dark500_light700">{title}</p>
+
               <Image
                 src="/icons/chevron-right.svg"
                 alt="Chevron"
@@ -44,14 +45,24 @@ const RightSidebar = () => {
             </Link>
           ))}
         </div>
-          </div>
-          <div className="mt-16">
-              <h3 className="h3-bold text-dark200_light900">Pupular Tags</h3>
+      </div>
 
-              <div>
-                  {popularTags.map(({_id, name, questions}) => (<TagCard key={_id} _id={_id} name={name} questions={questions} showCount compact />))}
-              </div>
-          </div>
+      <div className="mt-16">
+        <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+
+        <div className="mt-7 flex flex-col gap-4">
+          {popularTags.map(({ _id, name, questions }) => (
+            <TagCard
+              key={_id}
+              _id={_id}
+              name={name}
+              questions={questions}
+              showCount
+              compact
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
